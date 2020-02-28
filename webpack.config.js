2f37eb94ff2c910f9c9765c2ac6555db72fb7296
yannick van bladel <yannick.van.bladel@freshheads.com>
2020-02-28 09:18:41 +0100

    Update npm ignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,49 @@
-var path = require('path'),
-    ExtractTextPlugin = require('extract-text-webpack-plugin'),
-    HTMLWebpackPlugin = require('html-webpack-plugin'),
-    StylelintBarePlugin = require('stylelint-bare-webpack-plugin'),
-    autoprefixer = require('autoprefixer'),
-    webpack = require('webpack'),
-    lintFormatter = require('lint-formatter');
+var path = require("path"),
+    HTMLWebpackPlugin = require("html-webpack-plugin"),
+    StylelintBarePlugin = require("stylelint-bare-webpack-plugin"),
+    MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+    lintFormatter = require("lint-formatter");
 
 var sassOptions = {
-    includePaths: [
-        path.resolve(__dirname, 'node_modules')
-    ]
+    includePaths: [path.resolve(__dirname, "node_modules")]
 };
 
 module.exports = {
-    entry : {
-        app: [path.join(__dirname, '/example.main.scss'), path.join(__dirname, '/docs.scss'), path.join(__dirname, '/docs.js')]
+    entry: {
+        app: [
+            path.join(__dirname, "/example.main.scss"),
+            path.join(__dirname, "/docs.scss"),
+            path.join(__dirname, "/docs.js")
+        ]
     },
     output: {
-        path: path.resolve(__dirname,'docs'),
-        filename: 'app.js'
+        path: path.resolve(__dirname, "docs"),
+        filename: "app.js"
     },
     module: {
         rules: [
             {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [{
-                        loader: 'css-loader'
-                    },
+                test: /\.s?css$/,
+                use: [
                     {
-                        loader: 'postcss-loader'
-                    },
-                    {
-                        loader: 'sass-loader',
+                        loader: MiniCssExtractPlugin.loader,
                         options: sassOptions
-                    }]
-                })
-            }, {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [{
-                        loader: 'css-loader'
-                    }, {
-                        loader: 'postcss-loader'
-                    }]
-                })
+                    },
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
             }
         ]
     },
     plugins: [
         new HTMLWebpackPlugin({
-            filename: 'index.html',
-            template: path.resolve(__dirname, 'index.html')
+            filename: "index.html",
+            template: path.resolve(__dirname, "index.html")
         }),
-        new ExtractTextPlugin('[name].css'),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
         new StylelintBarePlugin({
             formatter: lintFormatter.stylelint
-        }),
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                postcss: [
-                    autoprefixer({
-                        browsers: [
-                            'last 2 version',
-                            'ie >= 9',
-                            'ios 8'
-                        ]
-                    }),
-                ]
-            }
         })
     ]
 };
